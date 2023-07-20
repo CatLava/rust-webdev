09c1bdca0ae4a29c02448e3e27c63a0d52f569ee
Evan Schoening <evan.schoening@gmail.com>
2023-07-20 07:30:48 -0700

    improvements to web app

--- ch3/src/main.rs
+++ ch3/src/main.rs
@@ -1,7 +1,54 @@
 use warp::Filter;
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct QuestionId(String);
+
+#[derive(Debug, Serialize)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+impl Question {
+    fn new(
+        id: QuestionId,
+        title: String,
+        content: String,
+        tags: Option<Vec<String>>
+    ) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId("1".to_string()),
+        "first question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!("faq".to_string())),
+    );
+    Ok(warp::reply::json(
+        &question
+    ))
+}
 #[tokio::main]
 async fn main() {
-    let hello = warp::path("hello").map(|| format!("hellow"));
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let routes = get_items;
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030))
+        .await;
 }
